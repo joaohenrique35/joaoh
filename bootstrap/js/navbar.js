9a3ba35daaaf14ad52c049db55c141bf91faf901
joaohenrique35 <83438320+joaohenrique35@users.noreply.github.com>
2022-06-16 10:32:22 -0300

    Add files via upload

---
+++ bootstrap/js/navbar.js
@@ -0,0 +1,7 @@
+$('#collapse-navbar').on('show.bs.collapse', function(){
+    $('.joaohenrique').css('transform', 'translate(-50%, 10%');
+})
+
+$('#collapse-navbar').on('hide.bs.collapse', function(){
+    $('.joaohenrique').css('transform', 'translate(-50%, -50%');
+})
